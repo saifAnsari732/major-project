21e420fed1f32ff9165a605f9b5cbfa38c1ebce9
saifAnsari732 <ansarisaifuddin732@gmail.com>
2026-02-18 11:12:10 +0530

    last2

--- src/pages/View-paper.jsx
+++ src/pages/View-paper.jsx
@@ -45,7 +45,7 @@ const ViewPaper = () => {
               onClick={handleZoomIn}
               className="text-white hover:text-blue-400 transition p-1"
               title="Zoom In"
-            >
+            >   
               <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v6m3-3H7" />
               </svg>
