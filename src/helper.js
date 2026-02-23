260bac2678ff52be76c1d989a3b61cc6eccea6cd
saifAnsari732 <ansarisaifuddin732@gmail.com>
2026-02-23 13:28:04 +0530

    set

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,4 @@
 
 
-export const VITE_API_URL = "http://localhost:5000/api";
-// export const VITE_API_URL = "https://major-project-back.onrender.com/api";
+// export const VITE_API_URL = "http://localhost:5000/api";
+export const VITE_API_URL = "https://major-project-back.onrender.com/api";
